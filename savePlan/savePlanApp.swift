c26ca1f4c26063b8b219a3c249b8e35cd78a3536
TakJewon <taknineball@castis.com>
2024-08-07 16:27:17 +0900

    Initial Commit

---
+++ savePlan/savePlanApp.swift
@@ -0,0 +1,17 @@
+//
+//  savePlanApp.swift
+//  savePlan
+//
+//  Created by 탁제원 on 8/7/24.
+//
+
+import SwiftUI
+
+@main
+struct savePlanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

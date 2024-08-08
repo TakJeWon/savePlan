560268c01eb48f42f4c8d92a4cd1c454fd169284
TakJewon <taknineball@castis.com>
2024-08-08 14:29:42 +0900

    start with main page

--- savePlan/ContentView.swift
+++ savePlan/ContentView.swift
@@ -10,12 +10,18 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+                Text("2024")
+                Grid(horizontalSpacing: 10, verticalSpacing: 10) {
+                    ForEach(0..<4) { _ in
+                        GridRow {
+                            ForEach(0..<3) { _ in
+                                Color("MonthCellColor")
+                            }
+                        }.frame(width: 110, height: 120)
+                    }
+                }
+                .padding()
         }
-        .padding()
     }
 }
 
